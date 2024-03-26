92e064277dbc2cbd4cc4a050c1f8f94ae92d3e63
Daemon Larson <aslplarson@gmail.com>
2024-03-26 13:50:26 -0700

    Began gluttony ring

---
+++ GluttonyRing/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace GluttonyRing
+{
+    internal sealed class ModEntry : Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+
+        }
+    }
+}
